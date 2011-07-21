c7a81f21b3ddd047cda3b33de5378222f47439e8
Daniele Ricci <daniele.athome@gmail.com>
2011-07-21 17:11:54 +0200

    Some minor fixes
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- src/org/kontalk/util/MessageUtils.java
+++ src/org/kontalk/util/MessageUtils.java
@@ -76,7 +76,7 @@ public final class MessageUtils {
         MessageDigest md;
         md = MessageDigest.getInstance("SHA-1");
         byte[] sha1hash = new byte[40];
-        md.update(text.getBytes("iso-8859-1"), 0, text.length());
+        md.update(text.getBytes(), 0, text.length());
         sha1hash = md.digest();
 
         return convertToHex(sha1hash);
