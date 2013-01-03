7c383091ab6d7d9e1a97a6a81a47ed7d0b25addb
Daniele Ricci <daniele.athome@gmail.com>
2013-01-03 20:27:47 +0100

    Temporary workaround for smiley popup resize over input method
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- src/org/kontalk/ui/PopupWindows.java
+++ src/org/kontalk/ui/PopupWindows.java
@@ -14,7 +14,7 @@ import android.content.Context;
 
 /**
  * Custom popup window.
- * 
+ *
  * @author Lorensius W. L. T <lorenz@londatiga.net>
  *
  */
@@ -24,10 +24,10 @@ public class PopupWindows {
 	protected View mRootView;
 	protected Drawable mBackground = null;
 	protected WindowManager mWindowManager;
-	
+
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param context Context
 	 */
 	public PopupWindows(Context context) {
@@ -39,41 +39,41 @@ public class PopupWindows {
 			public boolean onTouch(View v, MotionEvent event) {
 				if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
 					mWindow.dismiss();
-					
+
 					return true;
 				}
-				
+
 				return false;
 			}
 		});
 
 		mWindowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
 	}
-	
+
 	/**
 	 * On dismiss
 	 */
-	protected void onDismiss() {		
+	protected void onDismiss() {
 	}
-	
+
 	/**
 	 * On show
 	 */
-	protected void onShow() {		
+	protected void onShow() {
 	}
 
 	/**
 	 * On pre show
 	 */
 	protected void preShow() {
-		if (mRootView == null) 
+		if (mRootView == null)
 			throw new IllegalStateException("setContentView was not called with a view to display.");
-	
+
 		onShow();
 
-		if (mBackground == null) 
+		if (mBackground == null)
 			mWindow.setBackgroundDrawable(new BitmapDrawable());
-		else 
+		else
 			mWindow.setBackgroundDrawable(mBackground);
 
 		mWindow.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
@@ -81,13 +81,15 @@ public class PopupWindows {
 		mWindow.setTouchable(true);
 		mWindow.setFocusable(true);
 		mWindow.setOutsideTouchable(true);
+        // FIXME workout some better solution
+        mWindow.setInputMethodMode(PopupWindow.INPUT_METHOD_NOT_NEEDED);
 
 		mWindow.setContentView(mRootView);
 	}
 
 	/**
 	 * Set background drawable.
-	 * 
+	 *
 	 * @param background Background drawable
 	 */
 	public void setBackgroundDrawable(Drawable background) {
@@ -96,33 +98,33 @@ public class PopupWindows {
 
 	/**
 	 * Set content view.
-	 * 
+	 *
 	 * @param root Root view
 	 */
 	public void setContentView(View root) {
 		mRootView = root;
-		
+
 		mWindow.setContentView(root);
 	}
 
 	/**
 	 * Set content view.
-	 * 
+	 *
 	 * @param layoutResID Resource id
 	 */
 	public void setContentView(int layoutResID) {
 		LayoutInflater inflator = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		
+
 		setContentView(inflator.inflate(layoutResID, null));
 	}
 
 	/**
 	 * Set listener on window dismissed.
-	 * 
+	 *
 	 * @param listener
 	 */
 	public void setOnDismissListener(PopupWindow.OnDismissListener listener) {
-		mWindow.setOnDismissListener(listener);  
+		mWindow.setOnDismissListener(listener);
 	}
 
 	/**
