23ce3e274c8f57645e72cd18623ca04de1a90730
Daniele Ricci <daniele.athome@gmail.com>
2013-11-18 15:05:10 +0100

    Fix to request for indeterminate progress bar (appcompat)
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- src/org/kontalk/xmpp/ui/CodeValidation.java
+++ src/org/kontalk/xmpp/ui/CodeValidation.java
@@ -60,7 +60,7 @@ public class CodeValidation extends AccountAuthenticatorActionBarActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.code_validation_screen);
 
         setSupportProgressBarIndeterminate(true);
