f6047d89a5003390c27c8c374e47e4dca0b23038
Daniele Ricci <daniele.athome@gmail.com>
2014-07-05 22:54:51 +0200

    Load avatars asynchronously (closes #160)
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- src/org/kontalk/ui/ContactsListItem.java
+++ src/org/kontalk/ui/ContactsListItem.java
@@ -22,22 +22,16 @@ import org.kontalk.R;
 import org.kontalk.data.Contact;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.widget.QuickContactBadge;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
-public class ContactsListItem extends RelativeLayout {
+public class ContactsListItem extends AvatarListItem {
     //private static final String TAG = ConversationListItem.class.getSimpleName();
 
     private Contact mContact;
     private TextView mText1;
     private TextView mText2;
-    private QuickContactBadge mAvatarView;
-
-    static private Drawable sDefaultContactImage;
 
     public ContactsListItem(Context context) {
         super(context);
@@ -45,10 +39,6 @@ public class ContactsListItem extends RelativeLayout {
 
     public ContactsListItem(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        if (sDefaultContactImage == null) {
-            sDefaultContactImage = context.getResources().getDrawable(R.drawable.ic_contact_picture);
-        }
     }
 
     @Override
@@ -57,22 +47,17 @@ public class ContactsListItem extends RelativeLayout {
 
         mText1 = (TextView) findViewById(android.R.id.text1);
         mText2 = (TextView) findViewById(android.R.id.text2);
-        mAvatarView = (QuickContactBadge) findViewById(R.id.avatar);
 
         if (isInEditMode()) {
             mText1.setText("Test contact");
             mText2.setText("+393375423981");
-            mAvatarView.setImageDrawable(sDefaultContactImage);
-            mAvatarView.setVisibility(VISIBLE);
         }
     }
 
     public final void bind(Context context, final Contact contact) {
         mContact = contact;
 
-        mAvatarView.assignContactUri(contact.getUri());
-        mAvatarView.setImageDrawable(contact.getAvatar(context, sDefaultContactImage));
-        mAvatarView.setVisibility(VISIBLE);
+        loadAvatar(contact);
 
         mText1.setText(contact.getName());
         String text2 = contact.getStatus();
