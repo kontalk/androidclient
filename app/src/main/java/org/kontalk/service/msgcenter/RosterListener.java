f0f7516fedb1af1166c0d194e7a3e8447405105d
Daniele Ricci <daniele.athome@gmail.com>
2014-08-13 17:01:29 +0200

    Basic checkstyle configuration (close #195)
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- app/src/main/java/org/kontalk/service/msgcenter/RosterListener.java
+++ app/src/main/java/org/kontalk/service/msgcenter/RosterListener.java
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.kontalk.service.msgcenter;
 
 import static org.kontalk.service.msgcenter.MessageCenterService.ACTION_ROSTER;
