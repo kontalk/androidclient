1a16236f758871864a34c7b8dbf69fb8768b3e12
Andrea Cappelli <a.cappelli87@gmail.com>
2014-08-14 13:59:15 +0200

    Checkstyle fixes
    Signed-off-by: Andrea Cappelli <a.cappelli87@gmail.com>

--- app/src/main/java/org/kontalk/ui/AudioContentView.java
+++ app/src/main/java/org/kontalk/ui/AudioContentView.java
@@ -1,34 +1,38 @@
+/*
+ * Kontalk Android client
+ * Copyright (C) 2014 Kontalk Devteam <devteam@kontalk.org>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.kontalk.ui;
 
+import java.io.File;
+import java.util.regex.Pattern;
+
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.os.Handler;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.SeekBar;
 
 import org.kontalk.R;
 import org.kontalk.data.Contact;
 import org.kontalk.message.AudioComponent;
-import org.kontalk.message.AudioComponent;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.util.Timer;
-import java.util.logging.LogRecord;
-import java.util.logging.StreamHandler;
-import java.util.regex.Pattern;
 
 
 /**
