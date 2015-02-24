c33daa3e2724b6310779a2abb48736f6eafb1de5
Daniele Ricci <daniele.athome@gmail.com>
2015-02-24 22:35:07 +0100

    Fix minimum emoji drawer size
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- app/src/main/java/org/kontalk/ui/view/KeyboardAwareRelativeLayout.java
+++ app/src/main/java/org/kontalk/ui/view/KeyboardAwareRelativeLayout.java
@@ -133,13 +133,13 @@ public class KeyboardAwareRelativeLayout extends FrameLayout {
     public int getKeyboardLandscapeHeight() {
         return PreferenceManager.getDefaultSharedPreferences(getContext())
             .getInt("keyboard_height_landscape",
-                getResources().getDimensionPixelSize(R.dimen.min_emoji_drawer_height));
+                getResources().getDimensionPixelSize(R.dimen.emoji_drawer_default_height));
     }
 
     public int getKeyboardPortraitHeight() {
         return PreferenceManager.getDefaultSharedPreferences(getContext())
             .getInt("keyboard_height_portrait",
-                getResources().getDimensionPixelSize(R.dimen.min_emoji_drawer_height));
+                getResources().getDimensionPixelSize(R.dimen.emoji_drawer_default_height));
     }
 
     private void setKeyboardLandscapeHeight(int height) {
