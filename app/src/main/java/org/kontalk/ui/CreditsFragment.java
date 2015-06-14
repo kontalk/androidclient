03135e4f4720a784c61d6b4fdb8d8d487e4a01ea
Daniele Ricci <daniele.athome@gmail.com>
2015-06-14 18:28:11 +0200

    Very ugly credits screen (close #199)
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- app/src/main/java/org/kontalk/ui/CreditsFragment.java
+++ app/src/main/java/org/kontalk/ui/CreditsFragment.java
@@ -18,13 +18,24 @@
 
 package org.kontalk.ui;
 
-import org.kontalk.R;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.kontalk.R;
+import org.kontalk.util.MessageUtils;
 
 
 /**
@@ -35,7 +46,51 @@ public class CreditsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.about_credits, container, false);
+        View view = inflater.inflate(R.layout.about_credits, container, false);
+
+        TextView coredevs = (TextView) view.findViewById(R.id.coredevs);
+        TextView translators = (TextView) view.findViewById(R.id.translators);
+
+        InputStream in = null;
+        try {
+            in = getResources().openRawResource(R.raw.credits);
+
+            ByteArrayOutputStream bos = MessageUtils.readFully(in, 8192);
+            JSONTokener reader = new JSONTokener(bos.toString());
+            JSONObject root = (JSONObject) reader.nextValue();
+
+            JSONArray devteam = root.getJSONArray("devteam");
+            addCredits(coredevs, devteam);
+
+            JSONArray trans = root.getJSONArray("translators");
+            addCredits(translators, trans);
+        }
+        catch (Exception ignore) {
+        }
+        finally {
+            try {
+                if (in != null)
+                    in.close();
+            }
+            catch (IOException ignore) {
+            }
+        }
+
+        return view;
+    }
+
+    private void addCredits(TextView view, JSONArray list) throws JSONException {
+        StringBuilder text = new StringBuilder();
+        int c = list.length();
+        for (int i = 0; i < c; i++) {
+            JSONObject credit = list.getJSONObject(i);
+            text.append(credit.getString("name"))
+                .append("\n<")
+                .append(credit.getString("email"))
+                .append(">\n");
+        }
+
+        view.setText(text);
     }
 
 }
