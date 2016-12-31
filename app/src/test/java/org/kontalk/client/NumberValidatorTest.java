bd47f01205bfa6fe08e9ecb8aa849697563de77c
Daniele Ricci <daniele.athome@gmail.com>
2016-12-31 12:51:50 +0100

    Preparing some test code for #917
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

---
+++ app/src/test/java/org/kontalk/client/NumberValidatorTest.java
@@ -0,0 +1,15 @@
+package org.kontalk.client;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class NumberValidatorTest {
+
+    @Test
+    public void testFixNumber() throws Exception {
+        // TODO see https://github.com/kontalk/androidclient/issues/917
+    }
+
+}
