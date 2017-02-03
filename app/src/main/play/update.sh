d9a9d51ccc6132807537b803a3b392d3933e793c
Daniele Ricci <daniele.athome@gmail.com>
2017-02-03 10:38:45 +0100

    More checks on automatic Google Play listing update script
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- app/src/main/play/update.sh
+++ app/src/main/play/update.sh
@@ -6,7 +6,17 @@
 set -e
 
 DEFAULT_LOCALE="en-US"
-MAX_SHORT_DESC_LEN="80"
+MAX_TITLE_LEN=30
+MAX_SHORT_DESC_LEN=80
+MAX_FULL_DESC_LEN=4000
+MAX_WHATSNEW_LEN=500
+
+function extract_json() {
+    INFILE="$1"
+    KEY="$2"
+    cat ${INFILE} | perl -MJSON -Mutf8 -n0777 \
+     -E "binmode(STDIN, \":encoding(UTF-8)\"); binmode(STDOUT, \":encoding(UTF-8)\"); \$r = decode_json(\$_); print \$r->{${KEY}}" -
+}
 
 function check_lang() {
     INFILE="$1"
@@ -19,23 +29,35 @@ function check_lang() {
     [ -n "${SHORT_DESC}" ] && [ "${SHORT_DESC_LEN}" -le "${MAX_SHORT_DESC_LEN}" ] && [ -n "${FULL_DESC}" ]
 }
 
+function extract_check() {
+    INFILE="$1"
+    KEY="$2"
+    MAX_LEN="$3"
+
+    OUT=$(extract_json ${INFILE} ${KEY})
+    OUT_LEN=$(expr length "${OUT}")
+    [ -n "${OUT}" ] && [ "${OUT_LEN}" -le "${MAX_LEN}" ] && echo "${OUT}"
+}
+
 function copy_lang() {
     INFILE="$1"
     LOCALE="$2"
-    echo "Updating locale ${LOCALE}"
-    mkdir -p ${LOCALE}/listing
-    cat ${INFILE} | perl -MJSON -Mutf8 -n0777 \
-     -E 'binmode(STDIN, ":encoding(UTF-8)"); binmode(STDOUT, ":encoding(UTF-8)"); $r = decode_json($_); say $r->{title}' - \
-     >${LOCALE}/listing/title
-    cat ${INFILE} | perl -MJSON -Mutf8 -n0777 \
-     -E 'binmode(STDIN, ":encoding(UTF-8)"); binmode(STDOUT, ":encoding(UTF-8)"); $r = decode_json($_); say $r->{short_description}' - \
-     >${LOCALE}/listing/shortdescription
-    cat ${INFILE} | perl -MJSON -Mutf8 -n0777 \
-     -E 'binmode(STDIN, ":encoding(UTF-8)"); binmode(STDOUT, ":encoding(UTF-8)"); $r = decode_json($_); say $r->{full_description}' - \
-     >${LOCALE}/listing/fulldescription
-    cat ${INFILE} | perl -MJSON -Mutf8 -n0777 \
-     -E 'binmode(STDIN, ":encoding(UTF-8)"); binmode(STDOUT, ":encoding(UTF-8)"); $r = decode_json($_); say $r->{recent_changes}' - \
-     >${LOCALE}/whatsnew
+
+    TITLE=$(extract_check ${INFILE} "title" ${MAX_TITLE_LEN})
+    SHORT_DESC=$(extract_check ${INFILE} "short_description" ${MAX_SHORT_DESC_LEN})
+    FULL_DESC=$(extract_check ${INFILE} "full_description" ${MAX_FULL_DESC_LEN})
+    WHATSNEW=$(extract_check ${INFILE} "recent_changes" ${MAX_WHATSNEW_LEN})
+
+    if [ -n "${SHORT_DESC}" ] && [ -n "${FULL_DESC}" ]; then
+        echo "Updating locale ${LOCALE}"
+        mkdir -p ${LOCALE}/listing
+        [ -n "${TITLE}" ] && echo "${TITLE}" >${LOCALE}/listing/title
+        echo "${SHORT_DESC}" >${LOCALE}/listing/shortdescription
+        echo "${FULL_DESC}" >${LOCALE}/listing/fulldescription
+        [ -n "${WHATSNEW}" ] && echo "${WHATSNEW}" >${LOCALE}/whatsnew
+    else
+        echo "Skipping locale ${LOCALE}"
+    fi
 }
 
 copy_lang "../../../../dist/google_play.json" ${DEFAULT_LOCALE}
