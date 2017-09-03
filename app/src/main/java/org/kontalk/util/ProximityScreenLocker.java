ecdc8c603e257a0906a2bcb74c746a5c514b0c76
Daniele Ricci <daniele.athome@gmail.com>
2017-09-04 00:54:12 +0200

    Very cripple proximity sensor screen off fallback (fix #1043)
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

---
+++ app/src/main/java/org/kontalk/util/ProximityScreenLocker.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2014 The Simlar Authors.
+ *
+ * This file is part of Simlar. (https://www.simlar.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.kontalk.util;
+
+
+public interface ProximityScreenLocker
+{
+	void acquire();
+
+	/// Not immediately means we wait till the proximity sensor signals that the user is not near anymore
+	@SuppressWarnings("SameParameterValue")
+	void release(final boolean immediately);
+}
