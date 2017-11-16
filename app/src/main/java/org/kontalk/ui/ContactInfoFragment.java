04f14fbf42631f43566e9c974013db138ead0de5
Daniele Ricci <daniele@casaricci.it>
2017-11-16 14:39:42 +0100

    Fix checkstyle
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/src/main/java/org/kontalk/ui/ContactInfoFragment.java
+++ app/src/main/java/org/kontalk/ui/ContactInfoFragment.java
@@ -41,7 +41,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.kontalk.Kontalk;
 import org.kontalk.R;
