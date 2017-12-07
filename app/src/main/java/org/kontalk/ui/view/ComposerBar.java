76682c46c088755a42cde1a608b7dc3a08aac27b
Daniele Ricci <daniele@casaricci.it>
2017-12-07 14:07:32 +0100

    Clean imports
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/src/main/java/org/kontalk/ui/view/ComposerBar.java
+++ app/src/main/java/org/kontalk/ui/view/ComposerBar.java
@@ -39,7 +39,6 @@ import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
