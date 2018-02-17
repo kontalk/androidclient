da4d38bbb506bcd46a7c88a7c4ad0cf0dec78cfb
Daniele Ricci <daniele@casaricci.it>
2018-02-17 19:55:47 +0100

    Allow HTTP connections for upload (fix #1137)
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/src/main/java/org/kontalk/upload/HTPPFileUploadConnection.java
+++ app/src/main/java/org/kontalk/upload/HTPPFileUploadConnection.java
@@ -20,6 +20,7 @@ package org.kontalk.upload;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
@@ -52,7 +53,7 @@ public class HTPPFileUploadConnection implements UploadConnection {
     private final Context mContext;
     private final String mUrl;
 
-    private HttpsURLConnection currentRequest;
+    private HttpURLConnection currentRequest;
 
     private final static int CONNECT_TIMEOUT = 15000;
     private final static int READ_TIMEOUT = 40000;
@@ -94,7 +95,7 @@ public class HTPPFileUploadConnection implements UploadConnection {
             return null;
         }
         catch (Exception e) {
-            throw innerException("upload error", e);
+            throw new IOException("upload error", e);
         }
         finally {
             currentRequest.disconnect();
@@ -110,22 +111,19 @@ public class HTPPFileUploadConnection implements UploadConnection {
         }
     }
 
-    private IOException innerException(String detail, Throwable cause) {
-        return new IOException(detail, cause);
-    }
-
     @SuppressLint("AllowAllHostnameVerifier")
-    @SuppressWarnings("deprecation")
-    private void setupClient(HttpsURLConnection conn, long length, String mime, boolean acceptAnyCertificate)
+    private void setupClient(HttpURLConnection conn, long length, String mime, boolean acceptAnyCertificate)
         throws CertificateException, UnrecoverableKeyException,
         NoSuchAlgorithmException, KeyStoreException,
         KeyManagementException, NoSuchProviderException,
         IOException {
 
-        conn.setSSLSocketFactory(ClientHTTPConnection.setupSSLSocketFactory(mContext,
-            null, null, acceptAnyCertificate));
-        if (acceptAnyCertificate)
-            conn.setHostnameVerifier(new AllowAllHostnameVerifier());
+        if (conn instanceof HttpsURLConnection) {
+            ((HttpsURLConnection) conn).setSSLSocketFactory(ClientHTTPConnection.setupSSLSocketFactory(mContext,
+                null, null, acceptAnyCertificate));
+            if (acceptAnyCertificate)
+                ((HttpsURLConnection) conn).setHostnameVerifier(new AllowAllHostnameVerifier());
+        }
         conn.setRequestProperty("Content-Type", mime != null ? mime
             : "application/octet-stream");
         // bug caused by Lighttpd
@@ -146,11 +144,11 @@ public class HTPPFileUploadConnection implements UploadConnection {
     }
 
     /** A message posting method. */
-    private HttpsURLConnection prepareMessage(long length, String mime, boolean acceptAnyCertificate)
+    private HttpURLConnection prepareMessage(long length, String mime, boolean acceptAnyCertificate)
             throws IOException {
 
         // create uri
-        HttpsURLConnection conn = (HttpsURLConnection) new URL(mUrl).openConnection();
+        HttpURLConnection conn = (HttpURLConnection) new URL(mUrl).openConnection();
         try {
             setupClient(conn, length, mime, acceptAnyCertificate);
         }
