1060538c789acb23eee10b9de12d6c89418f5ba2
Daniele Ricci <daniele@casaricci.it>
2018-08-20 17:09:22 +0200

    Clean imports
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/src/main/java/org/kontalk/ui/ConversationsActivity.java
+++ app/src/main/java/org/kontalk/ui/ConversationsActivity.java
@@ -40,7 +40,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.text.TextUtils;
