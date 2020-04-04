76d4d7ed7fea71afbf654f35c39993461aca62a2
Daniele Ricci <daniele@casaricci.it>
2020-04-04 22:30:22 +0200

    Upgrading Gradle to latest version
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/src/main/java/org/kontalk/ui/CodeValidation.java
+++ app/src/main/java/org/kontalk/ui/CodeValidation.java
@@ -25,7 +25,8 @@ import java.util.Map;
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.google.i18n.phonenumbers.NumberParseException;
@@ -40,9 +41,11 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import android.os.IBinder;
 import android.view.MenuItem;
@@ -234,10 +237,9 @@ public class CodeValidation extends AccountAuthenticatorActionBarActivity {
 
             Glide.with(this)
                 .load(brandImage)
-                .listener(new RequestListener<String, GlideDrawable>() {
-
+                .listener(new RequestListener<Drawable>() {
                     @Override
-                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
                         brandParent.setVisibility(View.GONE);
                         brandView.setVisibility(View.GONE);
                         brandProgress.setVisibility(View.GONE);
@@ -250,7 +252,7 @@ public class CodeValidation extends AccountAuthenticatorActionBarActivity {
                     }
 
                     @Override
-                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
                         brandProgress.setVisibility(View.GONE);
                         return false;
                     }
